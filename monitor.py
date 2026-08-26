d5daa9fa4504bbe60665d8888f50115e732387b5
nazarshtein-commits <nazarshtein@gmail.com>
2026-08-26 14:44:18 +0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -8,7 +8,7 @@ SITES_TO_CHECK = [
 "https://swimtime.in.ua/meet/2997/",
 "https://swimtime.in.ua/meet/2998/",
 "https://swimtime.in.ua/meet/2999/",
-"https://swimtime.in.ua/meet/3000/",
+"https://swimtime.in.ua/meet/3000/"
 ]
 
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
